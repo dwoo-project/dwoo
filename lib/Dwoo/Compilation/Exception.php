4a19490e8b869696816e73acc6aec1a83254916d
Seldaek <Seldaek@0598d79b-80c4-4d41-97ba-ac86fbbd088b>
2008-08-25 20:53:40 +0000

    + API: Added Dwoo_Compilation_Exception methods getCompiler() and getTemplate() so you can catch the exception and use those to build a nicer error view with all the details you want
    git-svn-id: svn://dwoo.org/dwoo/trunk@157 0598d79b-80c4-4d41-97ba-ac86fbbd088b

--- lib/Dwoo/Compilation/Exception.php
+++ lib/Dwoo/Compilation/Exception.php
@@ -21,9 +21,23 @@
  */
 class Dwoo_Compilation_Exception extends Dwoo_Exception
 {
+	protected $compiler;
+	protected $template;
+
 	public function __construct(Dwoo_Compiler $compiler, $message)
 	{
-		$tpl = $compiler->getDwoo()->getTemplate();
-		parent::__construct('Compilation error at line '.$compiler->getLine().' in "'.$tpl->getResourceName().':'.$tpl->getResourceIdentifier().'" : '.$message);
+		$this->compiler = $compiler;
+		$this->template = $compiler->getDwoo()->getTemplate();
+		parent::__construct('Compilation error at line '.$compiler->getLine().' in "'.$this->template->getResourceName().':'.$this->template->getResourceIdentifier().'" : '.$message);
+	}
+
+	public function getCompiler()
+	{
+		return $this->compiler;
+	}
+
+	public function getTemplate()
+	{
+		return $this->template;
 	}
 }
