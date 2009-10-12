e756cf9ff13c42ffc12ebe7e626fcfc80547713e
seldaek <seldaek@0598d79b-80c4-4d41-97ba-ac86fbbd088b>
2009-10-12 20:00:24 +0000

    Fixed a bug when using the autoEscape feature with sub-templates (the compiled sub-template couldn't access the dwoo charset property, resulting in a fatal error) fixes #38
    git-svn-id: svn://dwoo.org/dwoo/trunk@288 0598d79b-80c4-4d41-97ba-ac86fbbd088b

--- tests/BlockTests.php
+++ tests/BlockTests.php
@@ -505,6 +505,20 @@ bleh();
 		$fixCall->init('');
 	}
 
+	public function testSubTemplatesWithAutoEscape()
+	{
+		$tpl = new Dwoo_Template_String('{load_templates "file:'.TEST_DIRECTORY.'/resources/templates.html"}{menu $menu}{noparam}{load_templates ""}');
+		$tpl->forceCompilation();
+		$this->compiler->setAutoEscape(true);
+		$this->assertEquals('<ul class="level0"><li>foo</li><li>bar</li><ul class="level1"><li>baz</li><li>qux</li></ul><li>boo</li><ul class="level1"><li>far</li><ul class="level2"><li>faz</li><li>mux</li></ul></ul><li>duck</li></ul>noparamoutput'."\n",
+			$this->dwoo->get($tpl, array('menu'=>array('foo', 'bar'=>array('baz','qux'), 'boo'=>array('far'=>array('faz','mux')), 'duck')), $this->compiler));
+		$this->compiler->setAutoEscape(false);
+
+		// fixes the init call not being called (which is normal)
+		$fixCall = new Dwoo_Plugin_template($this->dwoo);
+		$fixCall->init('');
+	}
+
 	public function testSubTemplatesMultiInc()
 	{
 		$tpl = new Dwoo_Template_File(TEST_DIRECTORY.'/resources/templateUsage.html');
