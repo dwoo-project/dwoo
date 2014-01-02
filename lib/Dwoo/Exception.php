204b690a7288fa516f20345913643873f929bf29
David <emulienfou@gmail.com>
2014-01-02 16:35:18 +0100

    Update Header comment

--- lib/Dwoo/Exception.php
+++ lib/Dwoo/Exception.php
@@ -2,17 +2,17 @@
 namespace Dwoo;
 
 /**
- * main dwoo exception class
+ * Main dwoo exception class
  *
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the use of this software.
  *
- * @author     Jordi Boggiano <j.boggiano@seld.be>
- * @copyright  Copyright (c) 2008, Jordi Boggiano
- * @license    http://dwoo.org/LICENSE   Modified BSD License
+ * @author     David Sanchez <david38.sanchez@gmail.com>
+ * @copyright  Copyright (c) 2013-2014 David Sanchez
+ * @license    http://dwoo.org/LICENSE GNU Lesser General Public License v3.0
  * @link       http://dwoo.org/
  * @version    2.0
- * @date       2013-09-01
+ * @date       2014-01-02
  * @package    Dwoo
  */
 class Exception extends \Exception {
