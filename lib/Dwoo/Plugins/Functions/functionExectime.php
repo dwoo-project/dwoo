d022f8eb24297e95697969fc799770783fda658c
David <emulienfou@gmail.com>
2014-01-21 22:47:46 +0100

    Update Exectime plugins's function

--- lib/Dwoo/Plugins/Functions/functionExectime.php
+++ lib/Dwoo/Plugins/Functions/functionExectime.php
@@ -9,9 +9,9 @@ use Dwoo\Core;
  * </pre>
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the use of this software.
- * @author     David Sanchez <david38.sanchez@gmail.com>
+ * @author     David Sanchez <david38sanchez@gmail.com>
  * @copyright  Copyright (c) 2013, David Sanchez
- * @license    http://dwoo.org/LICENSE   Modified BSD License
+ * @license    http://dwoo.org/LICENSE GNU Lesser General Public License v3.0
  * @link       http://dwoo.org/
  * @version    2.0
  * @date       2013-09-07
