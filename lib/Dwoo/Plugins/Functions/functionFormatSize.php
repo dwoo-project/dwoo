e3b93485976143a767f423c3821c03a290c0d9d0
David <emulienfou@gmail.com>
2014-01-26 17:08:12 +0100

    Update format_size plugin, add new features, see wiki for more information! Update function commented header

--- lib/Dwoo/Plugins/Functions/functionFormatSize.php
+++ lib/Dwoo/Plugins/Functions/functionFormatSize.php
@@ -1,11 +1,14 @@
 <?php
 namespace Dwoo\Plugins\Functions;
+
 use Dwoo\Core;
 
 /**
  * Formats a given filesize in bytes into a human readable filesize
  * <pre>
  *  * size : the filesize in bytes
+ *  * unit : the output unit desired
+ *  * decimals : round to display
  * </pre>
  *
  * This software is provided 'as-is', without any express or implied warranty.
@@ -16,29 +19,45 @@ use Dwoo\Core;
  * More information can be found here:
  * {@link http://www.gnu.org/copyleft/lesser.html}
  *
- * @author     Stephan Wentz <swentz@brainbits.net>
- * @copyright  Copyright (c) 2008, brainbits GmbH
- * @license    http://www.gnu.org/copyleft/lesser.html  GNU Lesser General Public License
+ * @author     David Sanchez <david38sanchez@gmail.com>
+ * @copyright  Copyright (c) 2014, David Sanchez
+ * @license    http://dwoo.org/LICENSE GNU Lesser General Public License v3.0
  * @link       http://dwoo.org/
  * @version    2.0
- * @date       2013-09-08
+ * @date       2014-01-26
  * @package    Dwoo
  */
-function functionFormatSize(Core $dwoo, $size) {
-	if (!$size) {
-		$size = 0;
+function functionFormatSize(Core $dwoo, $size, $unit = '', $decimals = 2) {
+	$units = array('B'  => 0,
+				   'KB' => 1,
+				   'MB' => 2,
+				   'GB' => 3,
+				   'TB' => 4,
+				   'PB' => 5,
+				   'EB' => 6,
+				   'ZB' => 7,
+				   'YB' => 8
+	);
+
+	$value = 0;
+	if ($size > 0) {
+		// Generate automatic prefix by bytes
+		// If wrong prefix given
+		if (!array_key_exists($unit, $units)) {
+			$pow  = floor(log($size) / log(1024));
+			$unit = array_search($pow, $units);
+		}
+
+		// Calculate byte value by prefix
+		$value = ($size / pow(1024, floor($units[$unit])));
 	}
 
-	$size     = (int)$size;
-	$prefixes = array("KB", "MB", "GB");
-	$prefix   = 'Byte';
-
-	while (sizeof($prefixes) && $size / 1024 > 1) {
-		$size /= 1024;
-		$prefix = array_shift($prefixes);
+	// If decimals is not numeric or decimals is less than 0
+	// then set default value
+	if (!is_numeric($decimals) || $decimals < 0) {
+		$decimals = 2;
 	}
 
-	$result = round($size) . " " . $prefix;
-
-	return $result;
+	// Format output
+	return sprintf('%.' . $decimals . 'f ' . $unit, $value);
 }
