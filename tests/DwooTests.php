95e22da871e954c68a64afd2f13df2c5f048d010
DSanchez <david38sanchez@gmail.com>
2016-09-13 15:50:01 -0400

    Update PHPUnit config file, remove autoload from composer direct requirement

--- tests/DwooTests.php
+++ tests/DwooTests.php
@@ -1,16 +1,6 @@
 <?php
-
-error_reporting(E_ALL|E_STRICT);
-if (!ini_get('date.timezone'))
-	date_default_timezone_set('CET');
-
 define('DWOO_CACHE_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR.'cache');
 define('DWOO_COMPILE_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR.'compiled');
-
-require dirname(__FILE__) . '/../vendor/autoload.php';
-//require dirname(__FILE__) . '/../lib/Dwoo.compiled.php';
-//set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__).'/../lib/');
-
 define('TEST_DIRECTORY', dirname(__FILE__));
 
 class DwooTests extends PHPUnit_Framework_TestSuite
