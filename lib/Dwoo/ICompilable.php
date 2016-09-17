37af75100ca0263b2ba102a27be1347e47ff6e91
DSanchez <david38sanchez@gmail.com>
2016-09-17 16:55:49 -0400

    Refactoring code

--- lib/Dwoo/ICompilable.php
+++ lib/Dwoo/ICompilable.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Copyright (c) 2013-2016
+ *
+ * @category  Library
+ * @package   Dwoo
+ * @author    Jordi Boggiano <j.boggiano@seld.be>
+ * @author    David Sanchez <david38sanchez@gmail.com>
+ * @copyright 2008-2013 Jordi Boggiano
+ * @copyright 2013-2016 David Sanchez
+ * @license   http://dwoo.org/LICENSE Modified BSD License
+ * @version   1.2.4
+ * @date      2016-09-17
+ * @link      http://dwoo.org/
+ */
+
 namespace Dwoo;
 
 /**
@@ -8,17 +23,6 @@ namespace Dwoo;
  * ...)</em>, which replaces the <em>process()</em> method (that means <em>compile()</em> should have all arguments it
  * requires). This software is provided 'as-is', without any express or implied warranty. In no event will the authors
  * be held liable for any damages arising from the use of this software.
- *
- * @category  Library
- * @package   Dwoo
- * @author    Jordi Boggiano <j.boggiano@seld.be>
- * @author    David Sanchez <david38sanchez@gmail.com>
- * @copyright 2008-2013 Jordi Boggiano
- * @copyright 2013-2016 David Sanchez
- * @license   http://dwoo.org/LICENSE Modified BSD License
- * @version   Release: 1.2.4
- * @date      2016-10-16
- * @link      http://dwoo.org/
  */
 interface ICompilable
 {
