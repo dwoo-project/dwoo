12b19e0a47618effa64386605eaf041d7c2af2d4
DSanchez <david38sanchez@gmail.com>
2016-09-17 21:04:40 -0400

    Fix plugins

--- lib/Dwoo/Plugins/Blocks/PluginElseif.php
+++ lib/Dwoo/Plugins/Blocks/PluginElseif.php
@@ -25,7 +25,7 @@ use Dwoo\ICompilable\Block as ICompilableBlock;
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the use of this software.
  */
-class PluginElseif extends BlockIf implements ICompilableBlock, IElseable
+class PluginElseif extends PluginIf implements ICompilableBlock, IElseable
 {
     /**
      * @param array $rest
