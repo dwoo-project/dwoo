102819a727d0b647a8744191b4ddcf4359d54513
DSanchez <david38sanchez@gmail.com>
2016-09-20 17:21:37 -0400

    Fix name for auto-generated function, remove namespace

--- lib/Dwoo/Plugins/Blocks/PluginTemplate.php
+++ lib/Dwoo/Plugins/Blocks/PluginTemplate.php
@@ -10,7 +10,7 @@
  * @copyright 2013-2016 David Sanchez
  * @license   http://dwoo.org/LICENSE Modified BSD License
  * @version   1.3.0
- * @date      2016-09-19
+ * @date      2016-09-20
  * @link      http://dwoo.org/
  */
 
@@ -95,7 +95,7 @@ class PluginTemplate extends BlockPlugin implements ICompilableBlock
         }
         $init .= '/* -- template start output */';
 
-        $funcName = 'Dwoo\Plugins\Functions\Plugin' . Core::toCamelCase($params['name']) . Core::toCamelCase($params['uuid']);
+        $funcName = 'Plugin' . Core::toCamelCase($params['name']) . Core::toCamelCase($params['uuid']);
 
         $search      = array('$this->charset', '$this->', '$this,',);
         $replacement = array('$dwoo->getCharset()', '$dwoo->', '$dwoo,',);
