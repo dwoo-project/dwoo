a94f4579cf8b28ef0ada65eea35022924b455204
DSanchez <david38sanchez@gmail.com>
2017-01-07 15:27:55 -0500

    Added the new `allowed_tags` parameter, update unit tests

--- lib/Dwoo/Plugins/Functions/PluginStripTags.php
+++ lib/Dwoo/Plugins/Functions/PluginStripTags.php
@@ -9,8 +9,8 @@
  * @copyright 2008-2013 Jordi Boggiano
  * @copyright 2013-2017 David Sanchez
  * @license   http://dwoo.org/LICENSE Modified BSD License
- * @version   1.3.2
- * @date      2017-01-06
+ * @version   1.3.3
+ * @date      2017-01-07
  * @link      http://dwoo.org/
  */
 
@@ -23,34 +23,33 @@ use Dwoo\Plugin;
 /**
  * Removes all html tags
  * <pre>
- *  * value : the string to process
- *  * addspace : if true, a space is added in place of every removed tag
+ *  * value: the string to process
+ *  * addspace: if true, a space is added in place of every removed tag
+ *  * allowable_tags: specify tags which should not be stripped
  * </pre>
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the use of this software.
- * TODO: add missing parameter `allowable_tags` - http://php.net/manual/en/function.strip-tags.php
- *
- * @param Compiler $compiler
- * @param string   $value
- * @param bool     $addspace
- *
- * @return string
  */
 class PluginStripTags extends Plugin implements ICompilable
 {
     /**
-     * @param Compiler $compiler
-     * @param string   $value
-     * @param bool     $addspace
+     * @param Compiler    $compiler
+     * @param string      $value
+     * @param bool        $addspace
+     * @param null|string $allowable_tags
      *
      * @return string
      */
-    public static function compile(Compiler $compiler, $value, $addspace = true)
+    public static function compile(Compiler $compiler, $value, $addspace = true, $allowable_tags = null)
     {
         if ($addspace === 'true') {
-            return "preg_replace('#<[^>]*>#', ' ', $value)";
+            if ("null" == $allowable_tags) {
+                return "preg_replace('#<[^>]*>#', ' ', $value)";
+            }
+
+            return "preg_replace('#<\\s*\\/?(" . $allowable_tags . ")\\s*[^>]*?>#im', ' ', $value)";
         }
 
-        return "strip_tags($value)";
+        return "strip_tags($value, $allowable_tags)";
     }
 }
